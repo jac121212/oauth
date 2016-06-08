4e2a65712a6a1e99ac63318d8ff034b86a1b459c
james.chan <james.chan@ultra-as.com>
2016-06-08 16:20:25 +0100

    Added oauth2

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var session = require('express-session');
 var passport = require('passport');
 var beerController = require('./controllers/beer');
 var userController = require('./controllers/user');
 var authController = require('./controllers/auth');
+var oauth2Controller = require('./controllers/oauth2');
 var clientController = require('./controllers/client');
 
 // Connect to the beerlocker MongoDB
@@ -14,10 +17,20 @@ mongoose.connect('mongodb://localhost:27017/beerlocker');
 // Create our Express application
 var app = express();
 
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
 // Use the body-parser package in our application
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
+}));
+
+// Use express session support since OAuth2orize requires it
+app.use(session({
+  secret: 'Super Secret Session Key',
+  saveUninitialized: true,
+  resave: true
 }));
 
 // Use the passport package in our application
@@ -46,6 +59,15 @@ router.route('/users')
 router.route('/clients')
   .post(authController.isAuthenticated, clientController.postClients)
   .get(authController.isAuthenticated, clientController.getClients);
+
+// Create endpoint handlers for oauth2 authorize
+router.route('/oauth2/authorize')
+  .get(authController.isAuthenticated, oauth2Controller.authorization)
+  .post(authController.isAuthenticated, oauth2Controller.decision);
+
+// Create endpoint handlers for oauth2 token
+router.route('/oauth2/token')
+  .post(authController.isClientAuthenticated, oauth2Controller.token);
 
 // Register all our routes with /api
 app.use('/api', router);
